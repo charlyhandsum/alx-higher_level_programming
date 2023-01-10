c9449efa3244c1dcdc332c5b9ca3219766c14154
Charles Ebong <charlesebong86@gmail.com>
2023-01-10 13:03:53 -0800

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""``Student`` class module"""
 
 
 class Student:
-    """Represent a student."""
+    """A class the represent a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """Sets attributes of a new Student instance
+
+            Args:
+                first_name (str): The first name
+                last_name (str): The last name
+                age (int): The age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        """Returns a json represent of Student"""
+        r = {}
+        if attrs is not None:
+            keys = [key for key in self.__dict__ if key in attrs]
+        else:
+            keys = [key for key in self.__dict__]
+
+        for key in keys:
+            value = getattr(self, key)
+            if type(value) in [list, dict, str, int, bool]:
+                r[key] = value
+        return r
